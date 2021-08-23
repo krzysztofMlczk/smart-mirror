56a4a3626029ccfc000c9a47ebe1f8d07f92d472
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-08-23 20:44:02 +0200

    SM-5.2 > Custom Stepper implementation

---
+++ src/renderer/components/register/CustomStepper.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import StepIcon from '@material-ui/core/StepIcon';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepConnector from '@material-ui/core/StepConnector';
+import { withStyles } from '@material-ui/core/styles';
+
+const CustomIcon = withStyles({
+  root: {
+    opacity: 0.5,
+    color: '#ffffff!important',
+    '& text': {
+      fill: '#000000',
+    },
+    '&$active': {
+      opacity: 1,
+    },
+    '&$completed': {
+      opacity: 1,
+    },
+  },
+  active: {},
+  completed: {},
+})(StepIcon);
+
+const CustomConnector = withStyles({
+  active: {
+    '& $line': {
+      opacity: 1,
+    },
+  },
+  completed: {
+    '& $line': {
+      opacity: 1,
+    },
+  },
+  line: {
+    opacity: 0.5,
+    borderColor: '#ffffff',
+    borderTopWidth: 2,
+  },
+})(StepConnector);
+
+const CustomStepper = ({ activeStepIndex, steps }) => {
+  return (
+    <Stepper
+      alternativeLabel
+      activeStep={activeStepIndex}
+      connector={<CustomConnector />}
+      style={{ backgroundColor: 'transparent' }}
+    >
+      {steps.map((step) => (
+        <Step key={step.bottomLabel}>
+          <StepLabel StepIconComponent={CustomIcon}>
+            {step.bottomLabel}
+          </StepLabel>
+        </Step>
+      ))}
+    </Stepper>
+  );
+};
+
+export default CustomStepper;
