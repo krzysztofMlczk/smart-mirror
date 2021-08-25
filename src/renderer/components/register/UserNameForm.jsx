b53a7eda01c64ab8dd16389ee90b8d70767f764a
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-08-26 00:15:07 +0200

    SM-5.1 > Implemented UserNameForm component

---
+++ src/renderer/components/register/UserNameForm.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+const UserNameForm = ({ next }) => {
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState(' ');
+
+  const userNameEmpty = username === '';
+  const errorOccured = error !== ' ';
+
+  const validate = (value) => {
+    // returns false if validation fails
+    const minLength = 5;
+    const maxLength = 37;
+    if (value.length < minLength) {
+      setError(`Username has to be at least ${minLength} characters long`);
+    } else if (value.length > maxLength) {
+      setError(`Username has to be less then ${maxLength} characters long`);
+    } else {
+      setError(' ');
+      return true;
+    }
+    return false;
+  };
+
+  const handleChange = (e) => {
+    // get rid of additional whitespaces
+    // (thanks to this user actually cannot enter whitespaces in username field)
+    const value = e.target.value.trim();
+    setUsername(value);
+    if (errorOccured) {
+      // validate dynamically when error already visible
+      validate(value);
+    }
+  };
+
+  const onNext = () => {
+    if (validate(username)) {
+      next();
+    }
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box textAlign="center">
+        <TextField
+          fullWidth
+          label="Username"
+          color="secondary"
+          error={errorOccured}
+          helperText={error}
+          autoFocus={false}
+          value={username}
+          onChange={handleChange}
+        />
+        <Button
+          variant="outlined"
+          color="secondary"
+          size="large"
+          disabled={userNameEmpty || errorOccured}
+          onClick={onNext}
+          endIcon={<ArrowForwardIosIcon />}
+          style={{ marginTop: '40px' }}
+        >
+          Next
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default UserNameForm;
