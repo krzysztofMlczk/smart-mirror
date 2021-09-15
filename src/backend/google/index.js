3c20c774730c8c159c95ae5113ed89b03a0aef1d
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-09-15 20:26:05 +0200

    SM-11.0 > Google Sign In v 1.1 hidden google keys

--- src/backend/google/index.js
+++ src/backend/google/index.js
@@ -1,10 +1,8 @@
 /* This code is based on https://blog.ecliptic.io/google-auth-in-electron-a47b773940ae */
 const { ipcRenderer } = require('electron');
 const qs = require('qs');
+const keys = require('./keys.json');
 
-const GOOGLE_CLIENT_ID =
-  '492199019764-ppjqtag3697cgcthsa07jletla52b47k.apps.googleusercontent.com';
-const GOOGLE_REDIRECT_URI = 'com.smartMirror:/oauth2Callback'; // more info on RedirectUris for installed/iOS apps: https://groups.google.com/forum/#!topic/oauth2-dev/P7bY0dThhuQ
 const GOOGLE_AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth'; // see: https://developers.google.com/identity/protocols/oauth2/native-app
 const GOOGLE_TOKEN_URL = 'https://www.googleapis.com/oauth2/v4/token'; // see: https://developers.google.com/identity/protocols/oauth2/native-app
 const GOOGLE_PROFILE_URL = 'https://www.googleapis.com/userinfo/v2/me'; // see: https://developers.google.com/identity/protocols/oauth2/native-app
@@ -13,8 +11,8 @@ async function signInWithPopup() {
   // TODO: Generate and validate PKCE code_challenge value
   const urlParams = {
     response_type: 'code',
-    redirect_uri: GOOGLE_REDIRECT_URI,
-    client_id: GOOGLE_CLIENT_ID,
+    redirect_uri: keys.redirectUri,
+    client_id: keys.clientId,
     scope: 'profile email',
   };
   const authUrl = `${GOOGLE_AUTHORIZATION_URL}?${qs.stringify(urlParams)}`;
@@ -30,8 +28,8 @@ async function fetchAccessTokens(code) {
     },
     body: JSON.stringify({
       code,
-      client_id: GOOGLE_CLIENT_ID,
-      redirect_uri: GOOGLE_REDIRECT_URI,
+      client_id: keys.clientId,
+      redirect_uri: keys.redirectUri,
       grant_type: 'authorization_code',
     }),
   }).then((data) => data.json());
