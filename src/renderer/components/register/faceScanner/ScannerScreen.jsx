8d64b369ca27090f8487d529e0f3edad5b42d0c9
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-09-29 22:43:51 +0200

    SM-5.5 > Implemented ScannerScreen Component

--- src/renderer/components/register/faceScanner/ScannerScreen.jsx
+++ src/renderer/components/register/faceScanner/ScannerScreen.jsx
@@ -1,37 +1,52 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Webcam from 'react-webcam';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
+  instruction: {
+    fontSize: '20px',
+    fontStyle: 'italic',
+  },
+  // helps to set dimensions of webcam video realatively to screen size
   webcamWrapper: {
     height: '70%',
     margin: '0 auto',
-    position: 'relative',
+    position: 'relative', // relative, so the centeringContainers can be positioned absolutely
+    display: 'flex',
+    alignItems: 'center',
+    overflow: 'hidden',
   },
-  overlayContainer: {
+  // centeringContainer helps to overlap overlay circle and CircularProgress to keep them in the same place
+  centeringContainer: {
     position: 'absolute',
     top: 0,
     left: 0,
     width: '100%',
     height: '100%',
-    overflow: 'hidden',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  // adds blending with background
+  blender: {
+    boxShadow: 'inset 0px 0px 100px black',
   },
   overlay: {
-    transformOrigin: 'center',
-    transform: 'translate(50%, 25%)',
+    borderRadius: '100%',
     // these below properties make it possible to keep aspect ration of the circle
     width: '50%',
     height: 0,
     paddingBottom: '50%',
     // ---------------------
-    borderRadius: '100%',
-    overflow: 'hidden',
-    boxShadow: '0px 0px 0px 1000px rgba(0, 0, 0, 0.5)',
+    position: 'relative', // scanner must have position relative to this element
+    overflow: 'hidden', // scanner should not display outside the overlay circle
+    boxShadow: '0px 0px 0px 1000px rgba(0, 0, 0, 0.7)', // blur background excluding circle in the middle
   },
+  // scanner bar class
   scanner: {
-    position: 'absolute',
+    position: 'absolute', // positioned relatively to overlay element
     top: '0',
     left: '0',
     width: '100%',
@@ -39,15 +54,17 @@ const useStyles = makeStyles({
     background: '#18c89b',
     boxShadow: '0 0 70px 20px #18c89b',
     clipPath: 'inset(0)',
-    // fade animation has to last 2 times longer than y animation (so it works)
+    // fade animation has to last 2 times longer than y animation (so it works properly)
     animation:
       '$y 1.2s ease-in-out infinite alternate, $fade 2.4s ease-in-out infinite',
   },
+  // scanner movement animation
   '@keyframes y': {
     to: {
       top: '100%',
     },
   },
+  // scanner shadow animation
   '@keyframes fade': {
     '33%': {
       clipPath: 'inset(-100px 0 0 0)',
@@ -70,58 +87,66 @@ const useStyles = makeStyles({
     textTransform: 'uppercase',
     letterSpacing: '2px',
     color: '#18c89b',
-    // animation: '$blinker 1s linear infinite',
   },
   circle: {
     display: 'inline-block',
     width: '15px',
     height: '15px',
-    borderRadius: '50%',
-    background: '#18c89b',
+    borderRadius: '100%',
+    background: '#ff0000',
     margin: '0 6px 2px 0',
     animation: '$blinker 1.2s linear infinite',
   },
   '@keyframes blinker': {
     '50%': { opacity: 0 },
   },
-  scanningProgress: {
-    position: 'absolute',
-    top: '25%',
-    left: '18.75%',
-    color: '#18c89b',
-  },
 });
 
 const ScannerScreen = () => {
+  const [webcamReady, setWebcamReady] = useState(false);
+  const overlayRef = useRef(null);
   const classes = useStyles();
+
   return (
     <>
       <Typography
         variant="overline"
-        display="block"
         align="center"
-        style={{ fontSize: '20px' }}
+        className={classes.instruction}
       >
         Place your face in the circle
       </Typography>
       <div className={classes.webcamWrapper}>
-        <div className={classes.overlayContainer}>
-          <div className={classes.overlay}>
-            <div className={classes.scanner} />
-          </div>
-          <div className={classes.scannerLabel}>
-            <span className={classes.circle} />
-            Scanning
-          </div>
-          <CircularProgress
-            variant="determinate"
-            size="50%"
-            thickness="0.5"
-            value={50}
-            className={classes.scanningProgress}
-          />
-        </div>
-        <Webcam height="100%" audio={false} />
+        {webcamReady ? (
+          <>
+            <div className={`${classes.centeringContainer} ${classes.blender}`}>
+              <div ref={overlayRef} className={classes.overlay}>
+                <div className={classes.scanner} />
+              </div>
+            </div>
+            <div className={classes.centeringContainer}>
+              <CircularProgress
+                variant="determinate"
+                color="secondary"
+                value={50}
+                thickness={0.6}
+                // needs the same size as overlay circle!
+                size={`${overlayRef?.current?.clientWidth}px`}
+              />
+            </div>
+            <div className={classes.scannerLabel}>
+              <span className={classes.circle} />
+              Scanning
+            </div>
+          </>
+        ) : (
+          <CircularProgress color="secondary" />
+        )}
+        <Webcam
+          height={webcamReady ? '100%' : '0%'}
+          audio={false}
+          onUserMedia={() => setWebcamReady(true)}
+        />
       </div>
     </>
   );
