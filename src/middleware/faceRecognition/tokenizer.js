3f6c66de5b432bd7256ef884eab977a33d72f625
Adrian Kiełbowicz <kielbowicz.adr@gmail.com>
2021-10-07 20:55:28 +0200

    SM-5.5 > Enhanced commnads enum to tokenizer

--- src/middleware/faceRecognition/tokenizer.js
+++ src/middleware/faceRecognition/tokenizer.js
@@ -1,22 +1,24 @@
+const commands = require('./commands');
+
 const tokenize = (data) => {
   const line = data.toString();
   // eslint-disable-next-line prefer-const
   let [command, value, code] = line.match(/(?:\\ |[^ ])+/g);
 
   switch (command) {
-    case 'progress':
+    case commands.PROGRESS:
       value = parseInt(value, 10);
       break;
-    case 'recognized':
+    case commands.RECOGNIZED:
       // don't need to parse (value already is a string)
       break;
-    case 'registered':
+    case commands.REGISTERED:
       // don't need to parse (value already is a string)
       break;
-    case 'error':
+    case commands.ERROR:
       // don't need to parse (value already is a string)
       break;
-    case 'fatal':
+    case commands.FATAL:
       value = `${value}; exitCode: ${code}`;
       break;
     default:
