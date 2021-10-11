05e6c3fcb1747b975e80c70d1ceb32bbd192e91a
KaczkaJestem <86444172+KaczkaJestem@users.noreply.github.com>
2021-10-11 22:01:28 +0200

    SM-19/Face-recognizer-launcher (#11)
    * SM-19.0 > Prepared scripts for port finding and launching the cpp module
    
    * SM-19.0 > Added script responsible for changing screen orientation
    
    * SM-19.0 > Created launcher function on electron side
    
    * SM-19.0 > Integrated launcher script with electron
    
    * SM-19.0 > Moved timeout to setupConnection method

--- src/middleware/faceRecognition/index.js
+++ src/middleware/faceRecognition/index.js
@@ -1,52 +1,70 @@
 const net = require('net');
 const commands = require('./commands');
 const tokenize = require('./tokenizer');
+const launchFaceRecognizer = require('./launcher/launcher');
 
 class FaceRecognition {
   constructor() {
     this.registeringProgressCallback = undefined;
     this.registeringSuccessCallback = undefined;
 
-    this.sender = net.createConnection({ port: 8081 }, () => {
-      // 'connect' listener.
-      /* eslint-disable no-console */
-      console.info('sender connected');
-    });
-
-    this.receiver = net.createConnection(
-      {
-        port: 8080,
-        onread: {
-          buffer: Buffer.alloc(255),
-          callback: (nread, data) => {
-            const msg = tokenize(data);
-
-            switch (msg.command) {
-              case commands.PROGRESS:
-                this.registeringProgressCallback(msg.value);
-                break;
-              case commands.REGISTERED:
-                this.registeringSuccessCallback();
-                break;
-              case commands.ERROR:
-                // TODO: fire error callback
-                break;
-              case commands.FATAL:
-                // TODO: fire error callback
-                break;
-              default:
-                break;
-            }
-          },
-        },
-      },
-      () => {
-        // connect listener
-        /* eslint-disable no-console */
-        console.info('revceiver connected');
-      }
-    );
+    launchFaceRecognizer()
+      .then((ports) => {
+        return this.setupConnection(ports);
+      })
+      /* eslint-disable promise/always-return */
+      .then((msg) => {
+        console.log(msg);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
+  setupConnection = (ports) => {
+    const [portSender, portReceiver] = ports;
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          this.sender = net.createConnection({ port: portSender });
+
+          this.receiver = net.createConnection({
+            port: portReceiver,
+            onread: {
+              buffer: Buffer.alloc(255),
+              callback: (nread, data) => {
+                const msg = tokenize(data);
+
+                switch (msg.command) {
+                  case commands.PROGRESS:
+                    this.registeringProgressCallback(msg.value);
+                    break;
+                  case commands.REGISTERED:
+                    this.registeringSuccessCallback();
+                    break;
+                  case commands.ERROR:
+                    // TODO: fire error callback
+                    break;
+                  case commands.FATAL:
+                    // TODO: fire error callback
+                    break;
+                  default:
+                    break;
+                }
+              },
+            },
+          });
+        } catch (error) {
+          reject(error);
+        }
+
+        resolve(
+          `Connection to face recognizer module set up succesfully.\nRunning on ports: ${portSender}, ${portReceiver}`
+        );
+      }, 1000);
+    });
+  };
 
   setProgressCallback = (progressCallback) => {
     this.registeringProgressCallback = progressCallback;
