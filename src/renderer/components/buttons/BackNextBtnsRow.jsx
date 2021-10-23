70a3a4a4f6fb19a3398f1d9c18159d11f1693cb4
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-10-23 16:38:54 +0200

    SM-21.2 > Extracted BtnsRow as a separate component

--- src/renderer/components/buttons/BackNextBtnsRow.jsx
+++ src/renderer/components/buttons/BackNextBtnsRow.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 
+import BtnsRow from './BtnsRow';
 import BackNextBtn from './BackNextBtn';
 
 const BackNextBtnsRow = ({ marginTop, onBack, isNextDisabled, onNext }) => {
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-between"
-      alignItems="center"
-      style={{ marginTop }}
-    >
+    <BtnsRow marginTop={marginTop}>
       <BackNextBtn variant="back" onClick={onBack}>
         Back
       </BackNextBtn>
       <BackNextBtn variant="next" disabled={isNextDisabled} onClick={onNext}>
         Next
       </BackNextBtn>
-    </Grid>
+    </BtnsRow>
   );
 };
 
