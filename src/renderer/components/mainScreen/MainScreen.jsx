2519cb096592b6fa20f92a3c8897317ec12054c3
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-10-26 23:49:32 +0200

    SM-25.1 > Updated MainScreen dummy to display all UserContext data

--- src/renderer/components/mainScreen/MainScreen.jsx
+++ src/renderer/components/mainScreen/MainScreen.jsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import UserContext from 'renderer/context/UserContext';
 
 const MainScreen = () => {
+  const { userData } = useContext(UserContext);
   return (
-    <div>
-      THIS IS A MAIN APPLICATION SCREEN
-      <br />
-      only registered users should see this screen
-    </div>
+    <>
+      {userData ? (
+        <div>
+          THIS IS A MAIN APPLICATION SCREEN
+          <br />
+          only registered users should see this screen
+          <br />
+          <br />
+          DATA FROM CONTEXT:
+          <br />
+          userName: {userData.userName}
+          <br />
+          <img
+            src={userData.avatar}
+            alt="Cannot Display"
+            style={{ width: '150px' }}
+          />
+          <br />
+          accessToken: {userData.accessToken}
+          <br />
+          expiresIn: {userData.expiresIn}
+          <br />
+          tokenType: {userData.tokenType}
+          <br />
+          idToken: {userData.idToken}
+          <br />
+          email: {userData.email}
+          <br />
+          locale: {userData.locale}
+          <br />
+          name: {userData.name}
+          <br />
+          <img
+            src={userData.picture}
+            alt="Cannot Display"
+            style={{ background: 'white' }}
+          />
+        </div>
+      ) : (
+        <CircularProgress color="secondary" />
+      )}
+    </>
   );
 };
 
