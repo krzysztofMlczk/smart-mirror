85c292beb7cdb3fe26300006284cba938c6f1922
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-10-26 23:48:30 +0200

    SM-25.1 > Implemented Logging in with face recognition (+ fetching relevant data into context during logIn)

--- src/renderer/components/loginScreen/LoginScreen.jsx
+++ src/renderer/components/loginScreen/LoginScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -7,6 +7,7 @@ import { Loading } from 'react-loading-dot';
 import { Link, useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
 
+import UserContext from 'renderer/context/UserContext';
 import Hint from './Hint';
 import GridContainer from '../layout/GridContainer';
 import ParticleComponent from '../visuals/ParticleComponent';
@@ -27,22 +28,81 @@ const useStyles = makeStyles({
 });
 
 const LoginScreen = () => {
-  const [recognized, setRecognized] = useState(null);
+  const [recognizedUserId, setRecognizedUserId] = useState(null);
   const classes = useStyles();
   const history = useHistory();
+  const { setUserData } = useContext(UserContext);
 
+  /**
+   * FOR DEBUG: simulate recognizing the user of this id
+   */
   useEffect(() => {
-    window.middleware.faceRecognition.recognize(setRecognized);
+    const timer = setTimeout(
+      () => setRecognizedUserId('107622913887326078037'),
+      2000
+    );
+
+    return () => clearTimeout(timer);
   }, []);
 
+  /**
+   * This useEffect calls C++ face recognition module
+   * to begin recognizing the user flow
+   */
   useEffect(() => {
-    if (recognized) {
-      // TODO: get all user data from NeDB
-      // TODO: save all user data into react.context
-      // go to mainScreen
-      history.push('/mainscreen');
+    window.middleware.faceRecognition.recognize(setRecognizedUserId);
+  }, []);
+
+  /**
+   * This useEffect:
+   * - will be called when user has been recognized (recognizedUserId is not null)
+   * - reads userName, avatar and refreshToken from DB (users collection)
+   * - fetches new accessToken (valid for 1h) using refreshToken from DB
+   * - fetches dynamic Google Profile data using just received accessToken
+   * - saves all relevant data into react.Context (see UserContext.js for the context object structure)
+   */
+  useEffect(() => {
+    /* eslint-disable @typescript-eslint/naming-convention */
+    async function logIn() {
+      // GET USER FROM DB BY GOOGLE ID
+      const { userName, avatar, googleData } =
+        await window.middleware.db.users.readUserById(recognizedUserId);
+      // FETCH NEW ACCESS TOKEN
+      const { access_token, expires_in, token_type, id_token } =
+        await window.middleware.google.refreshAccessToken(
+          googleData.tokens.refreshToken
+        );
+      // FETCH DYNAMIC GOOGLE PROFILE DATA
+      const { email, locale, name, picture } =
+        await window.middleware.google.fetchGoogleProfile(access_token);
+      // SAVE ALL RELEVANT DATA INTO REACT.CONTEXT
+      setUserData({
+        userName,
+        avatar,
+        accessToken: access_token,
+        expiresIn: expires_in,
+        tokenType: token_type,
+        idToken: id_token,
+        email,
+        locale,
+        name,
+        picture,
+      });
     }
-  }, [recognized, history]);
+    if (recognizedUserId) {
+      try {
+        logIn();
+        history.push('/mainscreen'); // go to main screen on successful recognition
+      } catch (err) {
+        // TODO: Implement valid error handling
+        // There might be a lot of reasons why the logIn function might fail
+        // but the most probable is that the refreshToken is not valid anymore
+        // which happens when: https://developers.google.com/identity/protocols/oauth2#expiration
+        // to obtain new refreshToken user has to authenticate with credentials
+        history.push('/login-with-credentials');
+      }
+    }
+  }, [recognizedUserId, setUserData, history]);
 
   return (
     <>
