b2b53b0a81f56c5329565352d01bf0a4de6be0ac
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-12-04 12:01:50 +0100

    SM-28.3 > Added clock alignment based on column

--- src/renderer/components/widgets/clockWidget/DefaultClock.jsx
+++ src/renderer/components/widgets/clockWidget/DefaultClock.jsx
@@ -4,36 +4,58 @@ import Clock from 'react-live-clock';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-  root: {
-    height: 'inherit',
-    width: 'inherit',
-    overflowY: 'auto',
-    overflowX: 'hidden',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    // justifyContent: 'space-around',
-  },
-  date: {
-    opacity: '0.7',
-  },
-  hour: {},
-  timeZone: {
-    opacity: '0.5',
-  },
-});
+const getAlignment = (index) => {
+  let alignment;
+  switch (index) {
+    case 0:
+      alignment = 'start';
+      break;
+    case 1:
+      alignment = 'center';
+      break;
+    case 2:
+      alignment = 'flex-end';
+      break;
+    default:
+      alignment = 'center';
+  }
+  return alignment;
+};
+
+const useStyles = (props) =>
+  makeStyles({
+    root: {
+      height: 'inherit',
+      width: 'inherit',
+      overflowY: 'auto',
+      overflowX: 'hidden',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: getAlignment(props.horizontalIndex),
+    },
+    marginLR: {
+      margin: '0 5%',
+    },
+    date: {
+      opacity: '0.7',
+    },
+    hour: {},
+    timeZone: {
+      opacity: '0.5',
+    },
+  });
 
 const getCurrentTimeZone = () =>
-  Intl.DateTimeFormat().resolvedOptions().timeZone;
+  // Intl.DateTimeFormat().resolvedOptions().timeZone; -> this returns system's timezone
+  moment.tz.guess(); // this estimates actual user timezone
 
 const getCurrentDate = () =>
   moment(new Date()).format('dddd, MMMM D, YYYY').split(',').join(', ');
 
-const DefaultClock = ({ openOptionsDialog }) => {
+const DefaultClock = ({ horizontalIndex, openOptionsDialog }) => {
   const [timeZone] = useState(() => getCurrentTimeZone());
   const [currentDate, setCurrentDate] = useState(getCurrentDate);
-  const classes = useStyles();
+  const classes = useStyles({ horizontalIndex })();
 
   const checkDate = () => {
     const newDate = getCurrentDate();
@@ -42,12 +64,15 @@ const DefaultClock = ({ openOptionsDialog }) => {
 
   return (
     <div className={classes.root}>
-      <Typography variant="h5" className={classes.date}>
+      <Typography
+        variant="h5"
+        className={`${classes.date} ${classes.marginLR}`}
+      >
         {currentDate}
       </Typography>
       <Typography
         variant="h1"
-        className={classes.hour}
+        className={`${classes.hour} ${classes.marginLR}`}
         onClick={openOptionsDialog}
       >
         <Clock
@@ -57,7 +82,9 @@ const DefaultClock = ({ openOptionsDialog }) => {
           onChange={checkDate}
         />
       </Typography>
-      <Typography className={classes.timeZone}>{timeZone}</Typography>
+      <Typography className={`${classes.timeZone} ${classes.marginLR}`}>
+        {timeZone}
+      </Typography>
     </div>
   );
 };
