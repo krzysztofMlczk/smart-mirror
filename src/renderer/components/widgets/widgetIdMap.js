5591f1f00a41df8769c261fe7ce6ceba7340d8b8
krzysztofMlczk <56411566+krzysztofMlczk@users.noreply.github.com>
2021-12-04 11:55:04 +0100

    SM-28.10 > Implemented news feed widget

--- src/renderer/components/widgets/widgetIdMap.js
+++ src/renderer/components/widgets/widgetIdMap.js
@@ -1,29 +1,32 @@
 // IMPORT WIDGET ICONS
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import ChatIcon from '@material-ui/icons/Chat';
-import EuroSymbolIcon from '@material-ui/icons/EuroSymbol';
+// import ChatIcon from '@material-ui/icons/Chat';
+// import EuroSymbolIcon from '@material-ui/icons/EuroSymbol';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import RadioIcon from '@material-ui/icons/Radio';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
 // IMPORT WIDGET
 import CalendarWidget from './calendarWidget/CalendarWidget';
 import ClockWidget from './clockWidget/ClockWidget';
-import ComplimentWidget from './ComplimentWidget';
-import CurrenciesWidget from './CurrenciesWidget';
+// import ComplimentWidget from './ComplimentWidget';
+// import CurrenciesWidget from './CurrenciesWidget';
 import MailboxWidget from './mailboxWidget/MailboxWidget';
-import WeatherWidget from './WeatherWidget';
+import WeatherWidget from './weatherWidget/WeatherWidget';
 import RadioWidget from './radioWidget/RadioWidget';
+import NewsFeedWidget from './newsFeedWidget/NewsFeedWidget';
 
 /* --- IDs OF ALL AVAILABLE WIDGETS --- */
 export const widgetIds = {
   calendar: 'calendar',
   clock: 'clock',
-  compliment: 'compliment',
-  currencies: 'currencies',
+  // compliment: 'compliment',
+  // currencies: 'currencies',
   mailbox: 'mailbox',
   weather: 'weather',
   radio: 'radio',
+  news: 'news',
 };
 
 /* --- widgetId -> Icon MAPPING --- */
@@ -31,11 +34,12 @@ export const widgetIds = {
 export const widgetIdToIcon = {
   [widgetIds.calendar]: CalendarTodayIcon,
   [widgetIds.clock]: AccessTimeIcon,
-  [widgetIds.compliment]: ChatIcon,
-  [widgetIds.currencies]: EuroSymbolIcon,
+  // [widgetIds.compliment]: ChatIcon,
+  // [widgetIds.currencies]: EuroSymbolIcon,
   [widgetIds.mailbox]: MailOutlineIcon,
   [widgetIds.weather]: WbSunnyIcon,
   [widgetIds.radio]: RadioIcon,
+  [widgetIds.news]: AnnouncementIcon,
 };
 
 /* --- widgetId -> Component MAPPING --- */
@@ -43,9 +47,10 @@ export const widgetIdToIcon = {
 export const widgetIdToComponent = {
   [widgetIds.calendar]: CalendarWidget,
   [widgetIds.clock]: ClockWidget,
-  [widgetIds.compliment]: ComplimentWidget,
-  [widgetIds.currencies]: CurrenciesWidget,
+  // [widgetIds.compliment]: ComplimentWidget,
+  // [widgetIds.currencies]: CurrenciesWidget,
   [widgetIds.mailbox]: MailboxWidget,
   [widgetIds.weather]: WeatherWidget,
   [widgetIds.radio]: RadioWidget,
+  [widgetIds.news]: NewsFeedWidget,
 };
